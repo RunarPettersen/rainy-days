f591cfb78414d2e488678af6e28acfb79e2de5a9
Runar Pettersen <runpet02453@stud.noroff.no>
2024-09-05 18:06:06 +0200

    Update menu.js

--- js/utils/menu.js
+++ js/utils/menu.js
@@ -1,10 +1,17 @@
 export function setActiveLink() {
     const navLinks = document.querySelectorAll('.nav-link');
-    const currentPath = window.location.pathname.replace(/\/$/, '').toLowerCase();
+    let currentPath = window.location.pathname.replace(/\/$/, '').toLowerCase();
+
+    if (currentPath === '') currentPath = '/';
 
     navLinks.forEach(link => {
-        const linkPath = new URL(link.getAttribute('href'), window.location.origin).pathname.replace(/\/$/, '').toLowerCase();
+        let linkPath = link.getAttribute('href').replace(/\/$/, '').toLowerCase();
 
+        if (linkPath === './') linkPath = '/';
+        if (!linkPath.startsWith('/')) linkPath = '/' + linkPath;
+
+        linkPath = linkPath.replace(/\/$/, '').toLowerCase();
+        
         if (linkPath === currentPath) {
             link.classList.add('active');
         } else {
