f272d19ef1f1f92c6ce45aa52af2128576e8ed28
Runar Pettersen <runpet02453@stud.noroff.no>
2024-09-05 18:40:58 +0200

    Update menu.js

--- js/utils/menu.js
+++ js/utils/menu.js
@@ -2,10 +2,10 @@ export function setActiveLink() {
     const navLinks = document.querySelectorAll('.nav-link');
     let currentPath = window.location.pathname.replace(/\/$/, '').toLowerCase();
 
-    // Determine if running on GitHub Pages by checking if the pathname includes the repo name
+    // Detect if the current environment is GitHub Pages
     const isGitHubPages = currentPath.includes('/rainy-days');
 
-    // If on GitHub Pages, adjust the current path to exclude the base path '/rainy-days'
+    // Adjust current path if running on GitHub Pages by removing the base path '/rainy-days'
     if (isGitHubPages) {
         currentPath = currentPath.replace('/rainy-days', '');
     }
@@ -13,23 +13,15 @@ export function setActiveLink() {
     navLinks.forEach(link => {
         let linkPath = new URL(link.getAttribute('href'), window.location.origin).pathname.replace(/\/$/, '').toLowerCase();
 
-        // Adjust link paths to exclude '/rainy-days' when on GitHub Pages
+        // Adjust link paths to exclude '/rainy-days' if on GitHub Pages
         if (isGitHubPages) {
             linkPath = linkPath.replace('/rainy-days', '');
         }
 
-        // Handling root paths correctly for relative links like './' or '../'
-        if (link.getAttribute('href') === './' || link.getAttribute('href') === '/') {
-            if (currentPath.includes('/product')) {
-                linkPath = '/product';
-            } else if (currentPath.includes('/about')) {
-                linkPath = '/about';
-            } else {
-                linkPath = '/';
-            }
-        }
-
-        if (linkPath === currentPath) {
+        // Special handling for the home page
+        if ((link.getAttribute('href') === './' || link.getAttribute('href') === '/') && currentPath === '') {
+            link.classList.add('active');
+        } else if (linkPath === currentPath) {
             link.classList.add('active');
         } else {
             link.classList.remove('active');
