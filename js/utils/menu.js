bbfc67bfd5266e77043d604196e1aaf444cfb1b3
Runar Pettersen <runpet02453@stud.noroff.no>
2024-09-05 20:05:52 +0200

    Update menu.js

--- js/utils/menu.js
+++ js/utils/menu.js
@@ -5,14 +5,10 @@ export function setActiveLink() {
     navLinks.forEach(link => {
         const linkPath = new URL(link.href, window.location.origin).pathname.toLowerCase();
 
-        // Debugging logs to see the paths being compared
-        console.log(`Comparing: {link: '${linkPath}', current: '${currentPath}'}`);
-
-        // Explicitly match each link to its corresponding path
         if (
-            (linkPath === '/' && currentPath === '/') || // Home link
-            (linkPath === '/product/' && currentPath.includes('/product')) || // Products link
-            (linkPath === '/about/' && currentPath.includes('/about')) // About link
+            (linkPath === '/' && currentPath === '/') ||
+            (linkPath === '/product/' && currentPath.includes('/product')) ||
+            (linkPath === '/about/' && currentPath.includes('/about'))
         ) {
             link.classList.add('active');
         } else {
