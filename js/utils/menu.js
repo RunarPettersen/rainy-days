0e3ff9442a609237d57a9c7ee28353f40c8edacf
Runar Pettersen <runpet02453@stud.noroff.no>
2024-09-05 12:22:42 +0200

    Update on menu links

--- js/utils/menu.js
+++ js/utils/menu.js
@@ -1,9 +1,9 @@
 export function setActiveLink() {
     const navLinks = document.querySelectorAll('.nav-link');
-    const currentPath = window.location.pathname.replace(/\/$/, '');
+    const currentPath = window.location.pathname.replace(/\/$/, '').toLowerCase();
 
     navLinks.forEach(link => {
-        const linkPath = new URL(link.href).pathname.replace(/\/$/, '');
+        const linkPath = new URL(link.getAttribute('href'), window.location.origin).pathname.replace(/\/$/, '').toLowerCase();
 
         if (linkPath === currentPath) {
             link.classList.add('active');
