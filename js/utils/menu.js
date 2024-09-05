e4c594ea1c365b2e742a9dd7a4cb4b740730da8c
Runar Pettersen <runpet02453@stud.noroff.no>
2024-09-05 12:11:17 +0200

    Update menu.js

--- js/utils/menu.js
+++ js/utils/menu.js
@@ -1,13 +1,14 @@
 export function setActiveLink() {
     const navLinks = document.querySelectorAll('.nav-link');
-
-    const currentPath = window.location.pathname;
+    const currentPath = window.location.pathname.replace(/\/$/, '');
 
     navLinks.forEach(link => {
-        const linkPath = new URL(link.href).pathname;
+        const linkPath = new URL(link.href).pathname.replace(/\/$/, '');
 
         if (linkPath === currentPath) {
             link.classList.add('active');
+        } else {
+            link.classList.remove('active');
         }
     });
 }
